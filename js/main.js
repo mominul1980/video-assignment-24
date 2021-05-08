07503da53395e7ff37e1c52f72b1c95904bb002d
mominul1980 <81607421+mominul1980@users.noreply.github.com>
2021-05-09 00:18:04 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// bx slider
+$(document).ready(function(){
+  $('.slider_area').bxSlider();
+  $('#main_menu').meanmenu({ 
+  meanMenuContainer: '#mobile_menu',
+  meanScreenWidth: 991 ,
+});
+
+  
+});
